9c4e1da796225cd710368d1087e46bb8e19f0adb
adelelayoubi <adelelayoubi@gmail.com>
2026-05-22 14:19:36 +0200

    add math functions and test modul

--- Test.py
+++ Test.py
@@ -1,2 +1,23 @@
 # this is a test
+
+# hh
 print("hello world!")
+# math_functions.py
+
+def add(a, b):
+    """Addiert zwei Zahlen."""
+    return a + b
+
+def subtract(a, b):
+    """Subtrahiert b von a."""
+    return a - b
+
+def multiply(a, b):
+    """Multipliziert zwei Zahlen."""
+    return a * b
+
+def divide(a, b):
+    """Dividiert a durch b. Wirft einen Fehler, wenn b gegen 0 geht."""
+    if b == 0:
+        raise ValueError("Teilung durch Null ist mathematisch nicht erlaubt!")
+    return a / b

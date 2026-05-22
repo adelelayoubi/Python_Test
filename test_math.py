fbb1fa5b374b133b5a192e92d5e61f365088bafc
adelelayoubi <adelelayoubi@gmail.com>
2026-05-22 14:14:46 +0200

    add math functions and test modul

---
+++ test_math.py
@@ -0,0 +1,31 @@
+# test_math.py
+import unittest
+# Wir importieren unsere Funktionen aus der anderen Datei
+from Test import add, subtract, multiply, divide
+
+class TestMathFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-5, -5), -10)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3, 4), 12)
+        self.assertEqual(multiply(-2, 3), -6)
+        self.assertEqual(multiply(5, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-10, 2), -5)
+        
+        # Hier testen wir, ob die Funktion bei einer 0 wirklich den ValueError wirft
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()

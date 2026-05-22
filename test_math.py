2061fe0edd2fc34587e89f210817e5b4161aaeb2
adelelayoubi <adelelayoubi@gmail.com>
2026-05-22 14:59:17 +0200

    add failed test case

--- test_math.py
+++ test_math.py
@@ -7,7 +7,7 @@ class TestMathFunctions(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, 1), 2)
         self.assertEqual(add(-5, -5), -10)
 
     def test_subtract(self):
